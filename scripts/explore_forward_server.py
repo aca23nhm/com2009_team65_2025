3aa825cbfd0d53d5ffbb098dce459c74a338bc96
lottie <lottiesletterbox@protonmail.com>
2025-03-21 15:44:04 +0000

    removed commmented out code

--- scripts/explore_forward_server.py
+++ scripts/explore_forward_server.py
@@ -219,16 +219,6 @@ class ExploreForwardServer(Node):
             self.send_velocity_commands()
             time.sleep(0.1)
 
-        """ this code seems to not solve any issues and just wastes time
-        #make the robot turn itself to 0
-        vel_cmd = Twist()
-        vel_cmd.angular.z = self.ANG_VEL
-        self.vel_pub.publish(vel_cmd)
-        #while not within 4° of zero
-        while not (-0.0174533*2 < self.current_yaw < 0.0174533*2):
-            time.sleep(0.1) # 100ms
-        self.vel_pub.publish(Twist())
-        """       
         while (time.time() - start_time) < exploration_time:
             if goal_handle.is_cancel_requested:
                 goal_handle.canceled()
